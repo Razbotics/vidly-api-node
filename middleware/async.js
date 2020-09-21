ee4de33d5886e8629bb3fdececcdbc227376518e
Shubhankar <das.shubhankar97@gmail.com>
2020-09-22 00:58:13 +0530

    bug fixes

--- middleware/async.js
+++ middleware/async.js
@@ -1,4 +1,4 @@
-module.exprts = function asyncMiddleware(handle) {
+module.exports = function asyncMiddleware(handle) {
   return async (req, res, next) => {
     try {
       await handler(req, res);
